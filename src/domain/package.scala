77e753e503753503ae6dfad4b4967ee4d4ef83b5
Alejandro Zalazar <alejandrozalazar@gmail.com>
2014-11-24 16:41:28 -0300

    Package

---
+++ src/domain/package.scala
@@ -0,0 +1,30 @@
+import java.util.GregorianCalendar
+import scala.collection.immutable.HashSet
+
+package object domain {
+
+  def FiltroSucursal(sucursal: Sucursal)(
+    viajes: HashSet[Viaje]) = {
+    viajes.filter(_.sucursalOrigen.equals(sucursal))
+  }
+
+  def FiltroTransporte(transporte: Transporte)(
+    viajes: HashSet[Viaje]) = {
+    viajes.filter(_.transporte.equals(transporte))
+  }
+
+  def FiltroTipoEnvio(tipoEnvio: TipoEnvio)(
+    viajes: HashSet[Viaje]) = {
+    viajes.filter(_.envios.exists(_.tipoEnvio.equals(tipoEnvio)))
+  }
+
+  def FiltroFecha(desde: GregorianCalendar)(hasta: GregorianCalendar)(
+    viajes: HashSet[Viaje]) = {
+    viajes.filter { (viaje) => viaje.fechaSalida.compareTo(desde) >= 0 && viaje.fechaSalida.compareTo(hasta) <= 0 }
+  }
+
+  def FiltroOrigenDestino(origen: Sucursal)(destino: Sucursal)(
+    viajes: HashSet[Viaje]) = {
+    viajes.filter { (viaje) => viaje.sucursalOrigen.equals(origen) && viaje.sucursalDestino.equals(destino) }
+  }
+}
